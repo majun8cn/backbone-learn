7646bd3b308daa0c5ddde9d2e089d840095e8931
Jun Ma <majun8cn@gmail.com>
2014-10-22 21:03:54 -0700

    the REST man api

--- express-man/routes/mans.js
+++ express-man/routes/mans.js
@@ -1,9 +1,81 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Set up MongoDB and create the Man model
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/testData');
+var Man = require('../models/man');
+
+/* GET mans listing. http://localhost:3000/mans */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+    Man.find(function(err, mans) {
+        if (err)
+            res.send(err);
+
+        res.json(mans);
+    });
+});
+
+/* POST mans create a new Man object. http://localhost:3000/mans */
+router.post('/', function(req, res) {
+    var man = new Man();
+    man.name = req.body.name;
+    man.age = req.body.age;
+
+    man.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({
+            message: "man created"
+        });
+    });
+});
+
+/* GET one man object. http://localhost:3000/mans/id */
+router.get('/:manid', function(req, res) {
+    Man.findById(req.params.manid, function(err, man) {
+        if (err)
+            res.send(err);
+        res.json(man);
+    });
+});
+
+/* POST update one man object. http://localhost:3000/mans/id */
+router.post('/:manid', function(req, res) {
+    Man.findById(req.params.manid, function(err, man) {
+        if (err)
+            res.send(err);
+
+        man.name = req.body.name;
+        man.age = req.body.age;
+
+        man.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({
+                message: "man updated"
+            });
+        });
+
+    });
+});
+
+
+/* DELETE one man object. http://localhost:3000/mans/id */
+router.delete('/:manid', function(req, res) {
+    Man.remove({
+            _id: req.params.manid
+        },
+        function(err, man) {
+            if (err)
+                res.send(err);
+
+            res.json({
+                message: "man deleted"
+            });
+        });
 });
 
 module.exports = router;
